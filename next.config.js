c1cddacd6f86c590d761f5fc431234b905e89810
윤준석(YunJunSeok) <83647215+junseok0304@users.noreply.github.com>
2025-11-01 21:19:18 +0900

    fix: babelrc issue fix

--- next.config.js
+++ next.config.js
@@ -1,18 +1,25 @@
 const withTM = require('next-transpile-modules')(['three']);
-const withPlugins = require('next-compose-plugins');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+    enabled: process.env.ANALYZE === 'true',
 });
 const CompressionPlugin = require('compression-webpack-plugin');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack: config => {
-    config.plugins.push(new CompressionPlugin());
-    return config;
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+
+    webpack(config, { isServer }) {
+        if (!isServer) {
+            config.plugins.push(
+                new CompressionPlugin({
+                    algorithm: 'gzip',
+                    threshold: 8192,
+                })
+            );
+        }
+        return config;
+    },
 };
 
-module.exports = withPlugins([withTM, withBundleAnalyzer], nextConfig);
+module.exports = withBundleAnalyzer(withTM(nextConfig));
